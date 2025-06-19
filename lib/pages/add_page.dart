2620f02ecf0e3c2e9a88199c2889ab4822427a9b
Barry Song <524000659@qq.com>
2025-06-19 11:34:24 +0800

    feat(背景颜色): 统一背景颜色

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -538,6 +538,7 @@ class _AddPageState extends ConsumerState<AddPage>
       }
     });
     return Scaffold(
+      backgroundColor: const Color(0xFFF5F6FB),
       appBar: _isEditMode
           ? AppBar(
               backgroundColor: Colors.white,
